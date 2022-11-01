9263090eec6ee87986a57e3e3ce7f7c30f0ba18f
Jiahong Long <jiahong888long@gmail.com>
2022-11-01 15:05:11 -0700

    impls

--- cache.hh
+++ cache.hh
@@ -1,9 +1,11 @@
+#include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 enum class CacheMode {
   kAssociative,
+  kFullyAssociative,
   kDirectMapped,
 };
 enum class CacheResult {
@@ -13,9 +15,108 @@ enum class CacheResult {
   kCapacity,
 };
 
+inline std::ostream &operator<<(std::ostream &os, CacheResult value) {
+  switch (value) {
+  case CacheResult::kHit:
+    return os << "hit";
+    break;
+  case CacheResult::kCompulsory:
+    return os << "compulsory";
+    break;
+  case CacheResult::kConflict:
+    return os << "conflict";
+    break;
+  case CacheResult::kCapacity:
+    return os << "capacity";
+    break;
+  default:
+    assert(false);
+  }
+}
+
+namespace {
+
+class CacheBase {
+public:
+  CacheBase(int log_num_lines) : log_num_lines_{log_num_lines} {}
+  bool full(int query) {
+    auto it =
+        std::find(fully_associative_.begin(), fully_associative_.end(), query);
+    if (it == fully_associative_.end()) {
+      // didn't find
+      if (fully_associative_.size() >= (1 << log_num_lines_)) {
+        fully_associative_.erase(fully_associative_.begin());
+      }
+      fully_associative_.emplace_back(query);
+      return false;
+    } else {
+      // found
+      fully_associative_.erase(it);
+      fully_associative_.emplace_back(query);
+      return true;
+    }
+  }
+
+  bool check_is_compulsory(int query) {
+    if (seen_.find(query) != seen_.end()) {
+      return false;
+    } else {
+      seen_.insert(query);
+      return true;
+    }
+  }
+
+  using Set = std::unordered_set<int>;
+  using Vector = std::vector<int>;
+
+  int log_num_lines_;
+
+  Set seen_ = Set{};
+  Vector fully_associative_ = Vector{};
+};
+} // anonymous namespace
+
 template <CacheMode mode> class Cache;
 
-template <> class Cache<CacheMode::kAssociative> {
+template <> class Cache<CacheMode::kDirectMapped> : private CacheBase {
+public:
+  /***
+   * @param log_num_lines log-2 of the number of lines in the cache
+   * */
+  Cache(int log_num_lines) : CacheBase{log_num_lines} {}
+
+  CacheResult lookup(int query) {
+    auto tag = query >> log_num_lines_;
+    auto set_idx = (tag << log_num_lines_) ^ query;
+    auto is_compulsory = check_is_compulsory(query);
+    auto full_hit = full(query);
+
+    auto it = c_.find(set_idx);
+    if (it == c_.end()) {
+      // didn't find
+      assert(is_compulsory);
+      c_.emplace(std::make_pair(set_idx, tag));
+      return CacheResult::kCompulsory;
+    } else {
+      auto candidate = it->second;
+      if (candidate == tag) {
+        return CacheResult::kHit;
+      } else {
+        // miss
+        c_.erase(it);
+        c_.emplace(std::make_pair(set_idx, tag));
+        return (is_compulsory) ? CacheResult::kCompulsory
+               : (full_hit)    ? CacheResult::kConflict
+                               : CacheResult::kCapacity;
+      }
+    }
+  }
+
+private:
+  std::unordered_map<int, int> c_ = {};
+};
+
+template <> class Cache<CacheMode::kAssociative> : private CacheBase {
 public:
   /***
    * @param log_num_lines log-2 of the number of lines in the cache
@@ -23,8 +124,8 @@ public:
    * @param associativity the associativity of the cache
    * */
   Cache(int log_num_lines, int set_bits, int associativity)
-      : log_num_lines_{log_num_lines}, set_bits_{set_bits},
-        associativity_{associativity} {}
+      : CacheBase{log_num_lines}, set_bits_{set_bits}, associativity_{
+                                                           associativity} {}
   CacheResult lookup(int query) {
     auto tag = query >> set_bits_;
     auto set_idx = (tag << set_bits_) ^ query;
@@ -59,42 +160,9 @@ public:
   }
 
 private:
-  bool full(int query) {
-    auto it =
-        std::find(fully_associative_.begin(), fully_associative_.end(), query);
-    if (it == fully_associative_.end()) {
-      // didn't find
-      if (fully_associative_.size() >= (1 << log_num_lines_)) {
-        fully_associative_.erase(fully_associative_.begin());
-      }
-      fully_associative_.emplace_back(query);
-      return false;
-    } else {
-      // found
-      fully_associative_.erase(it);
-      fully_associative_.emplace_back(query);
-      return true;
-    }
-  }
-
-  bool check_is_compulsory(int query) {
-    if (seen_.find(query) != seen_.end()) {
-      return false;
-    } else {
-      seen_.insert(query);
-      return true;
-    }
-  }
-
-  using Set = std::unordered_set<int>;
-  using Vector = std::vector<int>;
   using Map = std::unordered_map<int, Vector>;
+  Map c_ = Map{};
 
-  int log_num_lines_;
   int set_bits_;
   int associativity_;
-
-  Set seen_ = Set{};
-  Vector fully_associative_ = Vector{};
-  Map c_ = Map{};
 };
--- main.cc
+++ main.cc
@@ -5,31 +5,27 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   Cache<CacheMode::kAssociative> c(4, 2, 4);
+  Cache<CacheMode::kDirectMapped> d(4);
   vector<int> accesses = {
       0, 64, 1, 1, 65, 2, 2, 66, 3, 3, 67, 4,
       4, 68, 5, 5, 69, 6, 6, 70, 7, 7, 71, 8,
   };
+
+  cout << "----- 4-way associative -----" << endl;
   for (auto a : accesses) {
     auto addr = a * 4;
     auto no_offset = addr >> 4;
     auto result = c.lookup(no_offset);
 
-    cout << a << '\t';
-    switch (result) {
-    case CacheResult::kHit:
-      cout << "hit" << endl;
-      break;
-    case CacheResult::kCompulsory:
-      cout << "compulsory" << endl;
-      break;
-    case CacheResult::kConflict:
-      cout << "conflict" << endl;
-      break;
-    case CacheResult::kCapacity:
-      cout << "capacity" << endl;
-      break;
-    default:
-      assert(false);
-    }
+    cout << a << '\t' << result << endl;
+  }
+
+  cout << endl << endl << "----- direct-mapped -----" << endl;
+  for (auto a : accesses) {
+    auto addr = a * 4;
+    auto no_offset = addr >> 4;
+    auto result = d.lookup(no_offset);
+
+    cout << a << '\t' << result << endl;
   }
 }
